63182b20a6bf00b39cf85d15c6647dc7989419b2
bfpronaga <ronaga@bridgesforpeace.com>
2025-12-23 16:39:49 +0200

    pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+});
+const nextConfig: NextConfig = withPWA({
+    /* config options here */
+});
 
 export default nextConfig;
