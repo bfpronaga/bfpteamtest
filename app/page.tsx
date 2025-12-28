630bee4ea61b06da0ced0764405d6c07db43eeeb
bfpronaga <ronaga@bridgesforpeace.com>
2025-12-28 20:57:23 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import OneSignal from "react-onesignal";
 
 export default function Home() {
+    const [isLoaggedIn, setIsLoaggedIn] = useState(false);
     useEffect(() => {
         (async () => {
             await OneSignal.init({
@@ -11,12 +12,18 @@ export default function Home() {
                 //     enable: true,
                 // },
             });
+            setIsLoaggedIn(!!OneSignal.User.PushSubscription.optedIn);
         })();
     });
+    const handleLogin = async () => {
+        await OneSignal.login("test2");
+    };
     return (
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                <div className="onesignal-customlink-container"></div>
+                <div className="onesignal-customlink-container">
+                    <button onClick={handleLogin}>Login</button>
+                </div>
             </main>
         </div>
     );
