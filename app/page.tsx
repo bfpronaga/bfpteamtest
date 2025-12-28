2adadbe59f51bcdf11917a2b10c3bd9f42cc5bc1
bfpronaga <ronaga@bridgesforpeace.com>
2025-12-28 20:59:10 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -21,9 +21,7 @@ export default function Home() {
     return (
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                <div className="onesignal-customlink-container">
-                    <button onClick={handleLogin}>Login</button>
-                </div>
+                <button onClick={handleLogin}>Login</button> <div className="onesignal-customlink-container"></div>
             </main>
         </div>
     );
